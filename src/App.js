51671e4bb8f08353940ba03383457e842939e7a5
unknown <saied2421998@gmail.com>
2021-11-16 17:37:57 +0200

    starting the project

--- src/App.js
+++ src/App.js
@@ -4,19 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" id="header">
+        <img id="header-img" src={logo} className="App-logo" alt="logo" />
+        <nav id="nav-bar">
+          <a href="https://www.google.com/">hi</a>
+          <a href="https://github.com/">sa</a>
+          <a href="https://www.microverse.org/">ie</a>
+        </nav>
+
       </header>
     </div>
   );
